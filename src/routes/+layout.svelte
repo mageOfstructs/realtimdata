e036cc8db85486fbbd8fcf6bc748e986272fb14e
jason <jason.puschnig@gmx.at>
2024-05-07 15:26:41 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import "../app.postcss";
+  import { FirebaseApp } from "sveltefire";
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from "firebase/firestore";
+  import { getAuth } from "firebase/auth";
+
+  // Initialize Firebase
+  const app = initializeApp(/* your firebase config */);
+  const firestore = getFirestore(app);
+  const auth = getAuth(app);
+</script>
+
+<FirebaseApp {auth} {firestore}>
+  <slot />
+</FirebaseApp>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Doc } from "sveltefire";
+  import { getFirestore } from "firebase/firestore";
+  import { initializeApp } from "firebase/app";
+  import { doc, setDoc } from "firebase/firestore";
+
+  const app = initializeApp(/* your firebase config */);
+  const firestore = getFirestore();
+</script>
+
+<Doc ref={"posts/id"} let:data>
+  <p>{data?.counter}</p>
+  <p slot="loading">Loading...</p>
+</Doc>
