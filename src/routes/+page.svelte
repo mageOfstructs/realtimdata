edac56e992f2c19b4adf5ff8ebd60e89d4e167cc
jason <jason.puschnig@gmx.at>
2024-05-08 07:47:41 +0200

    made it into a login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,37 @@
   import { doc, setDoc } from "firebase/firestore";
 
   const app = initializeApp(/* your firebase config */);
-  const firestore = getFirestore();
+  const firestore = getFirestore(app);
+
+  const DATA_PATH = "testing/123";
+
+  function updateDb(path: string, data: Object) {
+    setDoc(doc(firestore, path), data);
+  }
+
+  let inputObj = { name: "", password: "" };
 </script>
 
-<Doc ref={"posts/id"} let:data>
-  <p>{data?.counter}</p>
-  <p slot="loading">Loading...</p>
+<Doc ref={DATA_PATH} let:data>
+  <form action="" class="grid grid-rows-3 gap-3 place-content-center m-4">
+    <label class="label">Username: </label>
+    <input class="input" type="text" bind:value={inputObj.name} required />
+    <label class="label">Password: </label>
+    <input
+      class="input"
+      type="password"
+      bind:value={inputObj.password}
+      required
+    />
+    <button
+      class="btn variant-filled"
+      on:click={() => {
+        let obj = { ...data };
+        obj[inputObj.name] = inputObj.password;
+        updateDb(DATA_PATH, obj);
+        inputObj = { name: "", password: "" };
+      }}>Test</button
+    >
+  </form>
+  <p class="center" slot="loading">Loading...</p>
 </Doc>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,32 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join } from "path";
+import type { Config } from "tailwindcss";
+import { skeleton } from "@skeletonlabs/tw-plugin";
+
+import forms from "@tailwindcss/forms";
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}",
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    skeleton({
+      themes: {
+        preset: [
+          {
+            name: "skeleton",
+            enhancements: true,
+          },
+        ],
+      },
+    }),
+    forms,
+  ],
 } satisfies Config;
